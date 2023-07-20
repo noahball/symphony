c69ba357507c6649da173c065c2e14558655f89e
Noah <noah@noahball.com>
2023-07-20 20:08:37 +1200

    Oops, prioritise ENV port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000
 
 var auth = require('basic-auth')
 app.use(express.json());
