b432e87b6ffde9fda63ca6f1c43302e69d43f73a
Noah <noah@noahball.com>
2023-10-25 22:25:42 +1300

    Ability to exclude classes and subject codes

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -32,11 +32,13 @@ router.get('/manage', async function (req, res) {
   const subjectsStr = await client.get("subjectsData");
   const subjectsData = JSON.parse(subjectsStr);
 
+  if(!classData[req.query.class]) {
+    return res.status(404).send("Class not found.")
+  }
+
   // Substitute subject and teacher codes for names
-
-  console.log(classData[req.query.class])
-
   // Some classes don't have a teacher or code, prevent crashes and log error if they don't
+
   try {
     classData[req.query.class].code = subjectsData[classData[req.query.class].code].name;
     classData[req.query.class].teacher = teachersData[classData[req.query.class].teacher].name;
@@ -58,5 +60,50 @@ router.get('/manage', async function (req, res) {
   })
 
 });
+
+router.get('/manage/exclude-class', async function (req, res) {
+  if (!req.query.class) {
+    return res.status(400).send("Please provide a class.")
+  }
+
+  const exclClassesStr = await client.get("exclClasses");
+  if (!exclClassesStr) { // If there is no class data
+    var exclClasses = { names: [] }; // Create an empty object
+  } else {
+    var exclClasses = JSON.parse(exclClassesStr); // Parse the class data
+  }
+
+  if (!exclClasses.names.includes(req.query.class)) {
+    exclClasses.names.push(req.query.class);
+    client.set("exclClasses", JSON.stringify(exclClasses));
+  }
+
+  res.redirect('/manage?class=' + req.query.class)
+})
+
+router.get('/manage/exclude-subject', async function (req, res) {
+  if (!req.query.subject) {
+    return res.status(400).send("Please provide a subject.")
+  }
+
+  const exclSubjectsStr = await client.get("exclSubjects");
+  if (!exclSubjectsStr) { // If there is no class data
+    var exclSubjects = { subjects: [] }; // Create an empty object
+  } else {
+    var exclSubjects = JSON.parse(exclSubjectsStr); // Parse the subject data
+  }
+
+  const classStr = await client.get("classData");
+  const classData = JSON.parse(classStr);
+
+  const subjectCode = classData[req.query.subject].code;
+
+  if (!exclSubjects.subjects.includes(subjectCode)) {
+    exclSubjects.subjects.push(subjectCode);
+    client.set("exclSubjects", JSON.stringify(exclSubjects));
+  }
+
+  res.redirect('/manage?class=' + req.query.subject)
+})
 
 module.exports = router;
